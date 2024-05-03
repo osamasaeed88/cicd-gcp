4cddf175f3b1ce88b1197fd3807ce3ea9d05d0a0
Osama Saeed <96544955+osamasaeed88@users.noreply.github.com>
2024-05-03 19:36:58 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, I love Pakistan !!');
+  res.send('Hello World, I love Pakistan, Pakistan Zindabad !!');
 });
 
 app.listen(PORT, HOST);
